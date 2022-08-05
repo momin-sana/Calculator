1cc42027fec7afe03de06f41da6b150937a0fcdb
sanamomin17 <sana.m.momin@outlook.com>
2022-08-05 12:33:41 +0500

    Update Calculator.js

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,40 +1,33 @@
 import React from "react";
 import "./calculator.css";
 import CalScreen from "./CalScreen/CalScreen";
-import Keypad from "./Keypad/Keypad";
-// import { GetVal } from "./Keypad/Keypad";
-// import ComputationScreen from "./CalScreen/ComputationScreen/ComputationScreen";
-// import ResultScreen from "./CalScreen/ResultScreen/ResultScreen";
+import Keypad from "./CalScreen/Keypad/Keypad";
+import { ContextProvider } from "../Calculator/ContextProvider";
 
-class Calculator extends React.Component {
-    render() {
-
-        return (
-
-            // <GetVal>
-            <
-            div className = "container" >
-            <
-            div className = "calculator" >
-            <
-            div className = "c-wrapper" >
-            <
-            div className = "ctc c-type" >
-            <
-            h2 className = "display-5 fw-bolder text-center text-white p-3" >
-            Calculator <
-            /h2> <
-            CalScreen / >
-            <
-            Keypad / >
-            <
-            /div> <
-            /div> <
-            /div> <
-            /div>
-            // </GetVal>
-
-        );
-    }
+function Calculator() {
+    return ( <
+        ContextProvider >
+        <
+        div className = "container" >
+        <
+        div className = "calculator" >
+        <
+        div className = "c-wrapper" >
+        <
+        div className = "ctc c-type" >
+        <
+        h2 className = "display-5 fw-bolder text-center text-white p-3" >
+        Calculator { " " } <
+        /h2>{" "} <
+        CalScreen / >
+        <
+        Keypad / > { " " } <
+        /div>{" "} <
+        /div>{" "} <
+        /div>{" "} <
+        /div>{" "} <
+        /ContextProvider>
+    );
 }
+
 export default Calculator;
