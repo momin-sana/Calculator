d2b13f9092be6405ade684977beb4ed0a6237762
sanamomin17 <sana.m.momin@outlook.com>
2022-08-06 12:12:44 +0500

    Update ContextProvider.js

--- src/Calculator/ContextProvider.js
+++ src/Calculator/ContextProvider.js
@@ -1,30 +1,92 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 
-export const NumberContext = React.createContext();
+
+export const NumberContext = createContext();
 
 const ContextProvider = (props) => {
-    const [number, setNumber] = useState("");
-    const handleCompValue = (num) => {
-        try {
-            if (!number.includes('.') || num !== '.') {
-                setNumber(`${(number + num).replace('[/^0+/]', '')}`);
-            }
-        } catch (error) {
-            num.preventDefault();
-            setNumber("")
-        }
+        const [number, setNumber] = useState("");
+        const [storedNumber, setStoredNumber] = useState('');
+        const [functionType, setFunctionType] = useState('');
 
-    }
-    console.log("handleCompValue", handleCompValue);
-    console.log("number", number);
-    return ( <
-        NumberContext.Provider value = {
-            {
-                handleCompValue,
-                number
+        const handleCompValue = (e) => {
+            try {
+                if (!number.includes(".") || e !== ".") {
+                    setNumber(`${(number + e).replace("[/^0+/]", "")}`);
+                }
+            } catch (error) {
+                e.preventDefault();
+                setNumber("");
             }
-        } > { props.children } <
-        /NumberContext.Provider>
-    );
+        };
+        const handleSetStoredValue = () => {
+            setStoredNumber(number);
+            setNumber('');
+        };
+
+        const handleClearValue = () => {
+            setNumber('');
+            setStoredNumber('');
+            setFunctionType('');
+        };
+
+        const handleBackButton = () => {
+            if (number !== '') {
+                const deletedNumber = number.slice(0, number.length - 1);
+                setNumber(deletedNumber);
+            }
+        };
+
+        const handleSetCalcFunction = type => {
+            if (number) {
+                setFunctionType(type);
+                handleSetStoredValue();
+            }
+            if (storedNumber) {
+                setFunctionType(type);
+            }
+        };
+        const doMath = () => {
+                if (number && storedNumber) {
+                    switch (functionType) {
+                        case '+':
+                            setStoredNumber(`${Math.round(`${(parseFloat(storedNumber) + parseFloat(number)) * 100}`) / 100}`);
+          break;
+        case '-':
+          setStoredNumber(`${Math.round(`${(parseFloat(storedNumber) - parseFloat(number)) * 1000}`) / 1000}`);
+          break;
+        case '/':
+          setStoredNumber(`${Math.round(`${(parseFloat(storedNumber) / parseFloat(number)) * 1000}`) / 1000}`);
+          break;
+        case '*':
+          setStoredNumber(`${Math.round(`${parseFloat(storedNumber) * parseFloat(number) * 1000}`) / 1000}`);
+          break;
+        default:
+          break;
+      }
+      setNumber('');
+    }
+  };
+  console.log("handleCompValue", handleCompValue);
+  console.log("number", number);
+  console.log("props",props);
+  return (
+    <NumberContext.Provider
+      value={{
+        handleCompValue,
+        doMath,
+        functionType,
+        handleBackButton,
+        handleClearValue,
+        handleSetCalcFunction,
+        handleSetStoredValue,
+        setNumber,
+        storedNumber,
+        number,
+      }}
+    >
+      {" "}
+      {props.children}{" "}
+    </NumberContext.Provider>
+  );
 };
 export { ContextProvider };
