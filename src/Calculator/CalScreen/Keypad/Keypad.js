5eb3b55e4b5e647ad64287e3f9c40d5c3b6a4a35
sanamomin17 <sana.m.momin@outlook.com>
2022-08-06 12:12:41 +0500

    Update Keypad.js

--- src/Calculator/CalScreen/Keypad/Keypad.js
+++ src/Calculator/CalScreen/Keypad/Keypad.js
@@ -2,161 +2,128 @@ import React, { useContext, useState } from "react";
 import { NumberContext } from "../../ContextProvider";
 import "./keypad.css";
 
-export default function Keypad(keyValue) {
+export default function Keypad({ keyvalue }) {
     const { handleCompValue } = useContext(NumberContext);
     console.log("handleCompValue", handleCompValue);
+
 
     return ( <
         div className = "c-grid" >
         <
         button type = "button"
         className = "top-btn span-two"
-        keyValue = "ac"
+        keyvalue = "ac"
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         ac { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button type = "button"
         className = "top-btn "
-        keyValue = "%"
+        keyvalue = "%"
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         %
         <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button type = "button"
         className = "top-btn special "
-        keyValue = "/"
+        keyvalue = "/"
         onClick = {
-            () => handleCompValue(keyValue) } >
-        / <
-        /button>{" "}
-
-        <
+            () => handleCompValue(keyvalue) } >
+        /{" "} <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 7 }
+        keyvalue = { 7 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue = `${keyvalue}`) } >
         7 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 8 }
+        keyvalue = { 8 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         8 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 9 }
+        keyvalue = { 9 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         9 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "special"
-        keyValue = "*"
+        keyvalue = "*"
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         x { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 4 }
+        keyvalue = { 4 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         4 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 5 }
+        keyvalue = { 5 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         5 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 6 }
+        keyvalue = { 6 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         6 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "special"
-        keyValue = "-"
+        keyvalue = "-"
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         -
         <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 1 }
+        keyvalue = { 1 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         1 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 2 }
+        keyvalue = { 2 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         2 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = { 3 }
+        keyvalue = { 3 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         3 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "special"
-        keyValue = "+"
+        keyvalue = "+"
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         +
         <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "span-two normal"
-        keyValue = { 0 }
+        keyvalue = { 0 }
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         0 { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "normal"
-        keyValue = "."
+        keyvalue = "."
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         . { " " } <
-        /button>{" "}
-
-        <
+        /button>{" "} <
         button className = "special"
-        keyValue = "="
+        keyvalue = "="
         onClick = {
-            () => handleCompValue(keyValue) } >
+            () => handleCompValue(keyvalue) } >
         = { " " } <
-        /button> <
+        /button>{" "} <
         /div>
     );
 }
